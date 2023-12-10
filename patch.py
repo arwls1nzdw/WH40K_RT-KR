0751a7ec66fb5d7e371b3d41e64799e662015732
arwls1nzdw <arwls1nzdw@gmail.com>
2023-12-10 22:15:51 +0900

    추가: Dialogs 영문 병기

--- patch.py
+++ patch.py
@@ -12,3 +12,15 @@ for f in glob("patches/**/*-kr.json", recursive=True):
         data['strings'][k]['Text'] = v
 
 utils.write_json(data, "dist/koKR.json")
+
+# 영문 병기
+enGB = utils.load_json("dist/enGB.json")
+for f in [*glob("patches/World/Dialogs/**/*-kr.json", recursive=True), "patches/World/Dialogs-kr.json"]:
+    d = utils.load_json(f)
+    for k, v in d.items():
+        if k not in data['strings']:
+            continue
+        en = enGB['strings'][k]['Text']
+        data['strings'][k]['Text'] = f"{v}\n<size=95%>{en}</size>"
+
+utils.write_json(data, "dist/koKR_영문병기.json")
